4c119f3b849990a892b5a34fe2efa33d5fe9c52e
MrsNezuko <79158196+MrsNezuko@users.noreply.github.com>
2021-02-17 14:27:20 +0700

    Subscribe Naufal Gamersss

---
+++ NAUFAL/donasi.js
@@ -0,0 +1,28 @@
+const donasi = (Ig, name) => { 
+	return `       
+┏━━━━━━━━━━━━━━━━━━━━
+┃          𝗗𝗢𝗡𝗔𝗦𝗜  
+┣━━━━━━━━━━━━━━━━━━━━
+┣━⊱ *DONASI SEIKHLASNYA:)* ❉⊰━━✿
+┃  
+┣━⊱ *DANA*
+┣⊱ *085271163378*
+┣━⊱ *PULSA*
+┣⊱ *085265938750*
+┃
+┣━━━━━━━━━━━━━━━━━━━━
+┃  *BOT BY ${name}*
+┗━━━━━━━━━━━━━━━━━━━━
+Note:
+Jika Gamau Donasi Setidaknya Follow IG Ngab:D
+Kalo Dah Donasi Silahkan Invit Di Group Kalian:)
+Makasih:)
+
+PENGGANTI DONASI GAN!!!
+https://youtube.com/c/NaufalGamersss
+${Ig}
+
+`
+}
+
+exports.donasi = donasi
